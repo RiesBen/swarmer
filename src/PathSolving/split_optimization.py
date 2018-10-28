8585c308191eb8061e82cf1b86a60f8c8af8a102
Elena Labzina <Lenchickk@users.noreply.github.com>
2018-10-28 04:25:46 +0100

    Update split_optimization.py

--- src/PathSolving/split_optimization.py
+++ src/PathSolving/split_optimization.py
@@ -40,9 +40,103 @@ def get_my_work(my_index, packages_divided):
         my_work=packages_divided[my_index][0:3]
         for i in range(0, 3):
             del packages_divided[my_index][i]
-    optimal_path = list()
-    #optimal_path = optimal_path(G, my_work)
+    #optimal_path = list()
+    optimal_path = optimal_path(G, my_work)
     return my_work, optimal_path
+
+
+def optimal_path(G, targets):
+   total_cost = list()
+   shortest_path = list()
+   min_cost = 0
+
+   if len(targets) == 1:
+       return nx.dijkstra_path(G, 1, targets[0])
+
+   if len(targets) == 2:
+       total_cost.append(nx.dijkstra_path_length(G, 1, targets[0]) + nx.dijkstra_path_length(G, targets[0], targets[1]))
+       min_cost = total_cost[0]
+       min_path = 0
+       total_cost.append(nx.dijkstra_path_length(G, 1, targets[1]) + nx.dijkstra_path_length(G, targets[1], targets[0]))
+       if (min_cost>total_cost[1]):
+           min_cost = total_cost[1]
+           min_path = 1
+
+       if min_path == 0:
+           sublist1 = nx.dijkstra_path(G, targets[0], targets[1])
+           del sublist1[0]
+           shortest_path = [nx.dijkstra_path(G, 1, targets[0]) + sublist1]
+
+       if min_path == 1:
+           sublist1 = nx.dijkstra_path(G, targets[1], targets[0])
+           del sublist1[0]
+           shortest_path = [nx.dijkstra_path(G, 1, targets[1]) + sublist1]
+       return shortest_path
+
+   elif len(targets) == 3:
+       total_cost.append(nx.dijkstra_path_length(G, 1, targets[0]) + nx.dijkstra_path_length(G, targets[0], targets[1]) + nx.dijkstra_path_length(G, targets[1], targets[2]))
+       min_cost = total_cost[0]
+       min_path = 0
+       total_cost.append(nx.dijkstra_path_length(G, 1, targets[0]) + nx.dijkstra_path_length(G, targets[0], targets[2]) + nx.dijkstra_path_length(G, targets[2], targets[1]))
+       if (min_cost>total_cost[1]):
+           min_cost = total_cost[1]
+           min_path = 1
+       total_cost.append(nx.dijkstra_path_length(G, 1, targets[1]) + nx.dijkstra_path_length(G, targets[1], targets[0]) + nx.dijkstra_path_length(G, targets[0], targets[2]))
+       if (min_cost>total_cost[2]):
+           min_cost = total_cost[2]
+           min_path = 2
+       total_cost.append(nx.dijkstra_path_length(G, 1, targets[1]) + nx.dijkstra_path_length(G, targets[1], targets[2]) + nx.dijkstra_path_length(G, targets[2], targets[0]))
+       if (min_cost>total_cost[3]):
+           min_cost = total_cost[3]
+           min_path = 3
+       total_cost.append(nx.dijkstra_path_length(G, 1, targets[2]) + nx.dijkstra_path_length(G, targets[2], targets[0]) + nx.dijkstra_path_length(G, targets[0], targets[1]))
+       if (min_cost>total_cost[4]):
+           min_cost = total_cost[4]
+           min_path = 4
+       total_cost.append(nx.dijkstra_path_length(G, 1, targets[2]) + nx.dijkstra_path_length(G, targets[2], targets[1]) + nx.dijkstra_path_length(G, targets[1], targets[2]))
+       if (min_cost>total_cost[5]):
+           min_cost = total_cost[5]
+           min_path = 5
+
+       if min_path == 0:
+           sublist1 = nx.dijkstra_path(G, targets[0], targets[1])
+           sublist2 = nx.dijkstra_path(G, targets[1], targets[2])
+           del sublist1[0]
+           del sublist2[0]
+           shortest_path = [nx.dijkstra_path(G, 1, targets[0]) + sublist1 + sublist2]
+
+       if min_path == 1:
+           sublist1 = nx.dijkstra_path(G, targets[0], targets[2])
+           sublist2 = nx.dijkstra_path(G, targets[2], targets[1])
+           del sublist1[0]
+           del sublist2[0]
+           shortest_path = [nx.dijkstra_path(G, 1, targets[0]) + sublist1 + sublist2]
+if min_path == 2:
+           sublist1 = nx.dijkstra_path(G, targets[1], targets[0])
+           sublist2 = nx.dijkstra_path(G, targets[0], targets[2])
+           del sublist1[0]
+           del sublist2[0]
+           shortest_path = [nx.dijkstra_path(G, 1, targets[1]) + sublist1 + sublist2]
+       if min_path == 3:
+           sublist1 = nx.dijkstra_path(G, targets[1], targets[2])
+           sublist2 = nx.dijkstra_path(G, targets[2], targets[0])
+           del sublist1[0]
+           del sublist2[0]
+           shortest_path = [nx.dijkstra_path(G, 1, targets[1]) + sublist1 + sublist2]
+       if min_path == 4:
+           sublist1 = nx.dijkstra_path(G, targets[2], targets[0])
+           sublist2 = nx.dijkstra_path(G, targets[0], targets[1])
+           del sublist1[0]
+           del sublist2[0]
+           shortest_path = [nx.dijkstra_path(G, 1, targets[2]) + sublist1 + sublist2]
+       if min_path == 5:
+           sublist1 = nx.dijkstra_path(G, targets[2], targets[1])
+           sublist2 = nx.dijkstra_path(G, targets[1], targets[2])
+           del sublist1[0]
+           del sublist2[0]
+           shortest_path = [nx.dijkstra_path(G, 1, targets[2]) + sublist1 + sublist2]
+       return shortest_path
+
 
 
 '''
